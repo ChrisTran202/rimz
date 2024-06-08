6e4766ad8c7f10814902755bcc30aa2902bf45a6
Chris Tran <christran202@gmail.com>
2024-06-08 13:29:26 -0500

    test-update

--- src/components/Bgvideo.jsx
+++ src/components/Bgvideo.jsx
@@ -1,10 +1,10 @@
-import Clip from '../assets/clips/rimzclips.mp4'
+import Clip from '../assets/clips/rimzclipz.mp4'
 
 const Bgvideo = () => {
   return (
 
-    <div className=" flex flex-row items-center justify-center container-fluid p-0 ">
-        <video  autoPlay muted loop className='h-[70rem]'>
+    <div className=" flex flex-row items-center playsinline justify-center container-fluid pb-10 ">
+        <video  autoPlay muted loop className='w-[100vw]'>
           <source src={Clip} type="video/mp4" />
         </video>
       </div>
