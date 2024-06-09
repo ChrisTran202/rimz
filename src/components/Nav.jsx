d29fc30908a067cccb3db6168d5b96c2d5be2887
Chris Tran <christran202@gmail.com>
2024-06-09 10:45:29 -0500

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import logo from '../assets/images/wheellogo.png'
 const Nav = () => {
   return (
-    <div>
-      <div className='flex flex-row items-center text-center '>
+    <div className=' relative z-30 p-2 '>
+      <div className='flex flex-row flex-no-wrap items-center text-center justify-between '>
         
         <h2 className='text-1.5xl font-bold text-white '>
         <img src={logo} alt="logo" className='w-40' />
           Make it look good, going fast!
         </h2>
-        <ul className=' text-white flex flex-row gap-2  text-2xl text-center justify-center '>
+        <ul className=' text-white flex flex-row gap-2  text-2xl text-center '>
         <li>Home</li>
         <li>Shop</li>
         <li>Contact</li>
--- src/components/RimsList.jsx
+++ src/components/RimsList.jsx
@@ -67,9 +67,9 @@ export const RimsImages = [
 function  RimsList() {
 
   return (
-    <div className='flex justify-center h-auto'>
-      <div className='bg-[rgb(255,255,255)] w-[70vw] rounded-2xl  '>
-        <div className=' flex-col grid grid-cols-2 sm:grid-cols-3 gap-4 p-5 justify-center content-center '>
+    <div className='flex justify-center md:mt-[10vh] lg:mt-[50vh] p-10'>
+      <div className='bg-[rgb(255,255,255)] w-[70vw] rounded-2xl pt-5 '>
+        <div className=' flex-col grid grid-cols-2 sm:grid-cols-3 gap-4 p-5 pt-10   justify-center content-center '>
           { RimsImages.map((rim) => (
           <div key={rim.id} className=' flex flex-col text-center contents-center  text-xl font-semibold hover:scale-110 transition-all duration-500 cursor-pointer '>
             <img src={rim.img} alt={rim.title} className='h-[vh] w-[30vw] flex justify-center '/>
