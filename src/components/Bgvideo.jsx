87ae837a4362c1e2daa12cfdb2ff0155c2a85adb
Chris Tran <christran202@gmail.com>
2024-06-10 22:25:16 -0500

    update

--- src/components/Bgvideo.jsx
+++ src/components/Bgvideo.jsx
@@ -2,8 +2,8 @@ import Clip from '../assets/clips/rimzclipz-2.mp4'
 
 const Bgvideo = () => {
   return (
-    <div className='  hidden lg:block  '>
-    <div className=" flex flex-row items-center playsinline justify-center container-fluid  h-[20vh]  ">
+    <div className='  hidden lg:block'>
+      <div className=" flex flex-row items-center playsinline justify-center container-fluid  h-[20vh]  ">
         <video  autoPlay muted loop className='w-[100vw] absolute z-10'>
           <source src={Clip} type="video/mp4" />
         </video>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,15 @@ const Nav = () => {
     <div className=' relative z-30 p-2 '>
       <div className='flex flex-row flex-no-wrap items-center text-center justify-between '>
         
-        <h2 className='text-1.5xl font-bold text-white '>
-        <img src={logo} alt="logo" className='w-40' />
+        <h2 className='text-2xl font-bold m-5 text-white '>
+          <img src={logo} alt="logo" className='w-40 xl:50' /> 
           Make it look good, going fast!
         </h2>
-        <ul className=' text-white flex flex-row gap-2  text-2xl text-center '>
-        <li>Home</li>
-        <li>Shop</li>
-        <li>Contact</li>
-      </ul>
+        <ul className=' text-white flex flex-row gap-5 text-2xl text-center font-bold m-5  '>
+          <li className='hover:scale-110 transition-all duration-500 cursor-pointer hover:underline'>Home</li>
+          <li className='hover:scale-110 transition-all duration-500 cursor-pointer hover:underline'>Shop</li>
+          <li className='hover:scale-110 transition-all duration-500 cursor-pointer hover:underline'>Contact</li>
+        </ul>
       </div>
       
       
