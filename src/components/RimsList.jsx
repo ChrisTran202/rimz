26884ae36741d35aa7f14af1b30b2db1c2d085c0
Chris Tran <christran202@gmail.com>
2025-03-04 20:58:22 -0600

    CSS update

--- src/components/RimsList.jsx
+++ src/components/RimsList.jsx
@@ -89,15 +89,15 @@ function  RimsList() {
 
   return (
     <div className=' flex flex-col items-center  mt-[3rem] md:mt-[8rem] lg:mt-[20rem] md:p-10'>
-      <div className='margin'>
-        <p className='text-[#ffffff] text-[1.3rem] md:text-5xl lg:text-7xl pb-10 md:pb-[20vh] '>
+      <div className='flex flex-row text-center'>
+        <p className='text-[#ffffff] text-[1.3rem] w-[100vw] md:text-5xl lg:text-7xl  pb-[5vh] md:pb-[20vh] '>
           Beauty is in the eye of the beholder!
         </p>
       </div>
       <div className='bg-[rgb(255,255,255)] w-[80vw] rounded-2xl pt-5 border-red-900 border-2  '>
         <div className=' flex-col grid grid-cols-2 sm:grid-cols-3 gap-4 p-5 pt-10   justify-center content-center '>
           { RimsImages.map((rim) => (
-          <div key={rim.id} className=' rounded-2xl flex flex-col text-center contents-center  text-xl font-semibold hover:bg-[#8c8c8c] hover:scale-110 transition-all duration-500 ease-out cursor-pointer '>
+          <div key={rim.id} className='m-1 md:m-2 rounded-2xl flex flex-col text-center contents-center  text-xl font-semibold  hover:bg-[#8c8c8c] hover:scale-110 transition-all duration-500 ease-out cursor-pointer '>
             <img src={rim.img} alt={rim.title} className='h-[vh] w-[30vw]flex justify-center '/>
             <h1 className='text-[1.3rem] md:text-[1.8rem] pt-1 md:pt-3 tracking-tighter md:tracking-normal'>{rim.title}</h1>
               
@@ -105,12 +105,12 @@ function  RimsList() {
                   <h1 className=' font-[SchibstedGrotesk] weight-bold  text-[.6rem] md:text-[.8rem] tracking-[.02rem] md:tracking-normal w-[20vw] md:w-[23vw] m-5'>{rim.description}</h1>
                 </div>
                 <div>
-                  <select>
+                  <select className='border-2 border-[#000000] rounded-md w-auto text-[.8rem] ' >
                     {rim.sizes.map((size) => (
-                      <option className='weight-light' key={size}>{size}</option>
+                      <option className=' weight-light ' key={size}>{size}</option>
                     ))}
                   </select>
-                  <button className='background-[#000000] text-[#7d1e1c] text-sm md:text-[1.3rem] p-2 rounded-md'>Add To Cart</button>
+                  <button  className='tracking-[.01rem] text-[#7d1e1c] text-[13px] md:text-[1.3rem] p-2 rounded-md w-[13vw]'>Add To Cart</button>
                 </div>
               </div>
           
